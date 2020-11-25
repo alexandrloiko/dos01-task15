5ed11d868e9a249c614a3de6fd4b4b36ffebd1ed
alexandrloiko <64065314+alexandrloiko@users.noreply.github.com>
2020-11-25 16:30:16 +0300

    Update hw18.sh

--- hw18.sh
+++ hw18.sh
@@ -2,23 +2,23 @@
 
 echo -e "1) Тип ОС"\\n"2) Имя ос"\\n"3) Версия ядра"\\n"4) Ip адрес"\\n"5) LA за последние 15 минут"\\n"6) Время работы системы"\\n"7) Информацию об использованной RAM"\\n"8) Информацию об авторизованных пользователях"
 echo -n "Введите (exit = 0): " 
-read x
+read x                          ##ввод значения
 
-while [ $x -ne 0 ]
+while [ $x -ne 0 ]              ##выполения цикла для повторного запуска оператора выбора case
 do
-    case  $x in 
-        1) echo `uname -o` ;;
-        2) echo `lsb_release -a | grep Description | cut -d: -f2` ;;
-        3) echo `uname -v` ;;
-        4) echo -e "`ip a`""\\n" ;;
-        5) echo `cat /proc/loadavg | awk '{print $3}'` ;;
-        6) echo `uptime -p` ;;
-        7) echo -e `top -n 1 | head -4 | grep Mem | awk '{print $1" "$2": "$8" "9}'` ;;
-        8) echo `w -h` ;;
+    case  $x in                 ##выполенения case
+        1) echo `uname -o` ;;   ##Тип ОС
+        2) echo `lsb_release -a | grep Description | cut -d: -f2` ;;    ##Имя ос
+        3) echo `uname -v` ;;   ##Версия ядра
+        4) echo -e "`ip a`""\\n" ;; ##Ip адрес
+        5) echo `cat /proc/loadavg | awk '{print $3}'` ;;   ##LA за последние 15 минут
+        6) echo `uptime -p` ;;      ##Время работы системы
+        7) echo -e `top -n 1 | head -4 | grep Mem | awk '{print $1" "$2": "$8" "9}'` ;;     ##Информацию об использованной RAM
+        8) echo `w -h` ;;       ##Информацию об авторизованных пользователях
         *) echo "Не найдено" ;;
     esac
 echo "*******************************"
 echo -e "1) Тип ОС"\\n"2) Имя ос"\\n"3) Версия ядра"\\n"4) Ip адрес"\\n"5) LA за последние 15 минут"\\n"6) Время работы системы"\\n"7) Информацию об использованной RAM"\\n"8) Информацию об авторизованных пользователях"
 echo -n "Введите (exit = 0): " 
-read x
+read x      ##перезаписываем значение
 done
