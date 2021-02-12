aee7c427e56f32c116f688f6edea952ba04960fa
alexandrloiko <64065314+alexandrloiko@users.noreply.github.com>
2021-02-12 19:38:43 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
-number = int(input('Enter range: '))
+number = int(input('Enter range: '))  ##ввод
 number+=1
-for _ in range(number):
-    if _%2 == 0:
+for _ in range(number): ##цикл на количества чисел
+    if _%2 == 0:  ## проверка числа на деление на 2 по модулю, то есть четное ли оно число
         print(_,'is EVEN')
     else:
         print(_,'is ODD')
